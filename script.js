fd6d1566d5021ff1a3bab9a26c7d144fb968aaef
modugno-gabriele <modugno.gabriele@iisgalvanimi.edu.it>
2025-02-05 10:38:19 +0000

    punto 1

---
+++ script.js
@@ -0,0 +1,31 @@
+function startGame() {
+    myGameArea.start();
+    myGamePiece = new component(30, 30, "red", 10, 120);
+}
+
+var redSquare = {
+    width: 20,
+    height: 20,
+    x: 10,
+    y: 120,
+    color: "red"
+  };
+var myGameArea = {
+    canvas : document.createElement("canvas"),
+    start : function() {
+        this.canvas.width = 480;
+        this.canvas.height = 270;
+        this.context = this.canvas.getContext("2d");
+        document.body.insertBefore(this.canvas, document.body.childNodes[0]);      
+    }
+}
+function component(width, height, color, x, y) {
+    this.width = width;
+    this.height = height;
+    this.x = x;
+    this.y = y;    
+    ctx = myGameArea.context;
+    ctx.fillStyle = color;
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+}
+
